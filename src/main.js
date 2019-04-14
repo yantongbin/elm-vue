92142099df5d3bc48707c42e3a928631854732d1
yantongbin <1317541371@qq.com>
2019-04-14 14:17:41 +0800

    路由模块创建与引用

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'//引入的路由模块文件
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,//在项目全局中调用路由
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router.js
@@ -0,0 +1,4 @@
+//创建的路由模块,已将其引入到了main.js中
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
